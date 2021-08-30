4b2a55aab65e8c6e7e9a5a6d62b4eb83f0b5c5d7
NatashaMelnik <n.melnik1788@gmail.com>
2021-08-30 17:55:06 +0300

    today tasks display

--- src/components/today-tasks.js
+++ src/components/today-tasks.js
@@ -3,15 +3,14 @@ import { Link } from 'react-router-dom';
 import Todo from './todo';
 
 const getCurrentDate = () => {
-    return new Date;
+    return new Date();
 };
 
 const isTodayTodo = (todo) => {
     const todoDate = new Date(todo.due_date);
-    if(inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
-        
+    if(todoDate.setHours(0,0,0,0) === getCurrentDate().setHours(0,0,0,0)) {
+        return true;
     }
-
 };
 
 const TodayTasks = (props) => {
@@ -21,14 +20,15 @@ const TodayTasks = (props) => {
     return (
         <div className='today-container'>
 
-
             {lists.map((list, index) =>
                 <div key={index}>
                     <Link to={`/list/${list.name}`}>
                         <h2 className='today-title'>{list.name}</h2>
                     </Link>
                     {list.tasks.map((todo, index2) =>
-                        <Todo key={index2} todo={todo} props={props} />
+                        {if(isTodayTodo(todo)) {
+                           return <Todo key={index2} todo={todo} props={props} />
+                        }}
                     )}
                 </div>
             )}
